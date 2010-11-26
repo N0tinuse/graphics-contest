fbd24c82415a9a1f788b77c724a6e022071f5d3d
S <s>
2010-11-26 01:25:43 -0800

    {"type":"SWC", "runs":"1"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -649,7 +649,7 @@ public class GraphicsContest extends GraphicsProgram {
 		for (int i = 0; i < lifeLabels.length; i++) {
 			lifeLabels[i] = new GImage("placeholder.png");
 			lifeLabels[i].setLocation(getWidth() - 180 + i * 60, 0);
-			lifeLabels[i].setSize(100, 60);
+			lifeLabels[i].setSize(90, 50);
 			add(lifeLabels[i]);
 		}
 		bulletCounter = 0;
